988d9be88c131e18c157f09a10211d317d6d2bbe
pan.lu@mtime.com <pan.lu@mtime.com>
2018-10-30 15:17:05 +0800

    修改导航收回问题2

--- mobile/js/nav.js
+++ mobile/js/nav.js
@@ -81,7 +81,7 @@ navPage.prototype.initEvent = function () {
         e.stopPropagation();
     })
 
-    $("html").click(function () {
+    $(window).on("touch", function () {
         // 设置小三角旋转
         $('.nav-list li[class*="active"]').removeClass("rotate180");
         // 设置导航下拉框隐藏
