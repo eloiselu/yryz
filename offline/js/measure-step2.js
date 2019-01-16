e1f696dd9edf6197c9c572f643f86994e9678df0
pan.lu@mtime.com <pan.lu@mtime.com>
2019-01-16 18:50:21 +0800

    修改摄像头展示的布局

--- offline/js/measure-step2.js
+++ offline/js/measure-step2.js
@@ -12,13 +12,11 @@ measureStep2.prototype.init = function () {
         audio: false,
         video: {
             width: {ideal: 1280},
-            height: {ideal: 720},
-            facingMode: {exact: "environment"}
+            height: {ideal: 720}
         }
     };
     // 视频元素
     this.video;
-
 
     // 初始化摄像头
     this.initMediaDevices();
@@ -39,7 +37,18 @@ measureStep2.prototype.initMediaDevices = function () {
             that.video.srcObject = mediaStream;
 
             that.video.onloadedmetadata = function (e) {
+                console.log(e);
                 that.video.play();
+
+                // 获取视频的宽度
+                console.log(that.video.clientWidth);
+                console.log(that.video.clientHeight);
+                // 获取屏幕的宽度
+                console.log(document.body.clientWidth);
+
+                // 设置video的位置偏移
+                $(that.video).css({left: -((that.video.clientWidth -document.body.clientWidth) / 2)});
+                // $(that.video).offsetLeft((that.video.clientWidth -document.body.clientWidth) / 2);
             };
         })
         .catch(function (err) {
@@ -49,11 +58,23 @@ measureStep2.prototype.initMediaDevices = function () {
 
 //初始化事件
 measureStep2.prototype.initEvent = function () {
+    var that = this;
+
     $('#take').on('click', function () {
         // 使用canvas进行拍照
-        var canvas = document.getElementById('canvas')
-        canvas.getContext('2d').drawImage(video, 0, 0, 2494, 3776);
-        $('#picture').css('src', canvas.toDataURL("image/png"))
+        var canvas = document.getElementById('canvas');
+
+        // 获取video的宽高
+        // canvas.getContext('2d').drawImage(video, 0, 0, that.video.clientWidth, that.video.clientHeight);
+        // $('#picture').css('src', canvas.toDataURL("image/png"))
+
+
+        //绘制canvas图形
+        // canvas.getContext('2d').drawImage(video, 0, 0, that.video.clientWidth, that.video.clientHeight);
+        canvas.getContext('2d').drawImage(video, -432, -191, that.video.clientWidth, that.video.clientHeight);
+
+        //把canvas图像转为img图片
+        document.getElementById('picture').src = canvas.toDataURL("image/png");
     })
 };
 
