c6d6cc939b47b71bd31f55024b4f09160d837033
lupanpan <767416500@qq.com>
2019-01-16 13:54:35 +0800

    增加选择性别和发型

---
+++ offline/js/measure-step1.js
@@ -0,0 +1,24 @@
+/**
+ * Created by lupan on 2019/1/16.
+ */
+
+// function gender(){
+//
+//     if($(".gender").css("display")=='none'){//如果show是隐藏的
+//
+//         $("#show").css("display","block");//show的display属性设置为block（显示）
+//
+//     }else{//如果show是显示的
+//
+//         $("#show").css("display","none");//show的display属性设置为none（隐藏）
+//
+//     }
+//
+// }
+
+$(function(){
+    $("#gender").click(function(){ //显示连接
+        $("#gender-pink").show(function(){
+        })
+    })
+})
