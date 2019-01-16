c6241fbe2f769a1f5b0fd60c319ad8e2b3090ea2
pan.lu@mtime.com <pan.lu@mtime.com>
2019-01-16 14:30:21 +0800

    侧面测量拍照

--- offline/js/measure-step2.js
+++ offline/js/measure-step2.js
@@ -1,24 +1,61 @@
 /**
  * Created by lupan on 2019/1/15.
  */
-// 视频大小
-var constraints = {audio: true, video: {width: 24,height: 37}};
-// 开启视频
-navigator.mediaDevices.getUserMedia(constraints).then(function(mediaStream) {
-    console.log('getUserMedia:', mediaStream)
-    var video = document.querySelector('video');
-    video.srcObject = mediaStream;
-    video.onloadedmetadata = function(e) {
-        video.play();
+var measureStep2 = function () {
+    return this;
+}
+
+//初始化
+measureStep2.prototype.init = function () {
+    // 视频大小
+    this.constraints = {
+        audio: false,
+        video: true
+        // video: {
+        //     width: {ideal: 1280},
+        //     height: {ideal: 1280}
+        // }
     };
+    // 视频元素
+    this.video;
 
-    // 使用canvas进行拍照
-    var canvas = document.getElementById('canvas')
-    $('button').on('click', function () {
+
+    // 初始化摄像头
+    this.initMediaDevices();
+    //初始化事件
+    this.initEvent();
+};
+
+// 初始化摄像头
+measureStep2.prototype.initMediaDevices = function () {
+    var that = this;
+
+    // 开启视频
+    navigator.mediaDevices.getUserMedia(this.constraints).then(function (mediaStream) {
+        console.log('getUserMedia:', mediaStream);
+
+        that.video = document.querySelector('#video');
+        that.video.srcObject = mediaStream;
+        that.video.onloadedmetadata = function (e) {
+            that.video.play();
+        };
+
+    }).catch(function (err) {
+        console.log(err.name + ": " + err.message);
+    });
+};
+
+//初始化事件
+measureStep2.prototype.initEvent = function () {
+    $('#take').on('click', function () {
+        // 使用canvas进行拍照
+        var canvas = document.getElementById('canvas')
         canvas.getContext('2d').drawImage(video, 0, 0, 2494, 3776);
-        $('img').css('src', canvas.toDataURL("image/png"))
+        $('#picture').css('src', canvas.toDataURL("image/png"))
     })
+};
 
-}).catch(function(err) {
-    console.log(err.name + ": " + err.message);
+$(function () {
+    var measureStep2Obj = new measureStep2();
+    measureStep2Obj.init();
 });
