8c5b1b6fe6461153bb47ad768688e8a2f67d601c
lupanpan <767416500@qq.com>
2019-01-18 09:50:22 +0800

    .1.18修改页面问题

--- offline/json数据.js
+++ offline/json数据.js
@@ -3,17 +3,21 @@
  */
 
 
-var json = {
-    code: "1000",
-    msg: "success",
-    content: {
-        "data_id": "e34kkdjlsjd65l44",//数据的唯一主键
-        "ac": 4.23,//头部高度
-        "bc": 7.34,//颈部高度
-        "head_neck": 8.34,//头颈高度差
-        "head_shoulder": 8.55,//头肩高度差
-        "body_url": "http://",//身体曲线图
-        "avatar_url": "http://"//头像
-    }
+// var json = {
+//     code: "1000",
+//     msg: "success",
+//     content: {
+//         "data_id": "e34kkdjlsjd65l44",//数据的唯一主键
+//         "ac": 4.23,//头部高度
+//         "bc": 7.34,//颈部高度
+//         "head_neck": 8.34,//头颈高度差
+//         "head_shoulder": 8.55,//头肩高度差
+//         "body_url": "http://",//身体曲线图
+//         "avatar_url": "http://"//头像
+//     }
+// }
+// alert(json.code);
+
+function GET(url, options) {
+    return commonFetcdh(url, options, 'GET')
 }
-alert(json.code);
