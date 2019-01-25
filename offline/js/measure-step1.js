8a366cbd915a4869d7a31c73b080735ceec85822
lupanpan <767416500@qq.com>
2019-01-25 09:56:24 +0800

    增加本地存储选中男女、发型

--- offline/js/measure-step1.js
+++ offline/js/measure-step1.js
@@ -10,28 +10,37 @@ var measureStep1 = function () {
 measureStep1.prototype.init = function () {
     //初始化事件
     this.initEvent();
+
+    //从本地获取数据
+    this.bindData();
 };
+
+//从本地获取数据
+measureStep1.prototype.bindData = function () {
+    var chooseGender = localStorage.getItem("chooseGender");
+    var chooseHair = localStorage.getItem("chooseHair");
+    if(chooseGender){
+        $("#gender .choose[class='active']").addClass("active");
+    }
+}
 
 //初始化事件
 measureStep1.prototype.initEvent = function () {
     $('.choose').on('click', function () {
-        if($(this).hasClass('active')){
-            $(this).siblings().removeClass('active');
-        }
-        else{
-            $(this).addClass("active");
-        }
-        //接收男女、长中短发值
-        var chooseGender = $(this).attr("data_type");
-        var chooseHair = $(this).attr("data_type");
+        //添加选中、兄弟之间不选
+        $(this).addClass("active").siblings().removeClass("active");
 
-        if ((chooseGender = 1 && 2 ) || (chooseGender = !0)){
-            // $(location).attr("href","../view/measure-step2.html")
-            // window.location.href="../view/measure-step2.html";
-            console.log(chooseType = 1 || 2);
-        }else if('chooseType = 1 || 2 || 3'){
-            console.log("chooseType = 1 || 2 || 3");
+        //接收男女、长中短发值
+        var chooseGender = $("#gender .choose[class*='active']").attr("data-type");
+        var chooseHair = $("#hair .choose[class*='active']").attr("data-type");
+
+        if(chooseGender && chooseHair){
+            //存到本地存储
+            localStorage.setItem("chooseGender",chooseGender);
+            localStorage.setItem("chooseHair",chooseHair);
+            $(location).attr("href","../view/measure-step2.html");
         }
+
     })
 };
 
