1cdd67eb7c76e7d8b74ac57dbca05d655acbf041
lupanpan <767416500@qq.com>
2019-01-28 11:43:39 +0800

    材质详情页跳转到产品购物页面

--- offline/js/texture-detail.js
+++ offline/js/texture-detail.js
@@ -27,7 +27,8 @@ textureDetailPage.prototype.setData = function () {
             'color': 'Pink',
             'textureHead2': 'textureDetail-pink-head2.png',
             'feature': 'VCollection从日本精心选购的PE+TPE弹性体是一种热塑性弹性体材料，具有高强度高回弹性，环保无毒触感柔软等特点由于拥有舒适的手感，广泛用于人造皮肤通过欧盟EU-10/2011食品级测试标准及欧盟RoHS环保安全标准。 中医养生云：头凉脚热七分饱在睡眠时，头部保持低温有效降低大脑皮层活跃度利于大脑皮层休息。软管枕具有透气排热的特点，能够有效排出枕头内的热量，保持枕头内部温度提高睡眠质量。',
-            'suitableCrowd': '颈椎不舒服，长时间坐姿工作压力大年轻人'
+            'suitableCrowd': '颈椎不舒服，长时间坐姿工作压力大年轻人',
+            'link':'pink'
 
         },
         'cheese': {
@@ -39,7 +40,8 @@ textureDetailPage.prototype.setData = function () {
             'color': 'white',
             'textureHead2': 'textureDetail-cheese-hardness.png',
             'feature': 'VCollection Team精心选择的“天然芝士乳胶条”（天然乳胶含量达百分之八十）是乳胶枕品类中全新的呈现形式整块的乳胶被冲压成条状，保持了所有乳胶的优势的同时可以最方便的取出和填充给你最方便的高度调节方式，弹性好，有乳胶独特气味，很好的吸湿性和透气性无污染无毒',
-            'suitableCrowd': '颈椎不舒服，长时间坐姿工作压力大年轻人'
+            'suitableCrowd': '颈椎不舒服，长时间坐姿工作压力大年轻人',
+            'link':'cheese'
         },
         'lightgray': {
             'name': '白色TPE弹体',
@@ -50,7 +52,8 @@ textureDetailPage.prototype.setData = function () {
             'color': 'White',
             'textureHead2': 'textureDetail-lightgray-hardness.png',
             'feature': 'PE+TPE弹性体是一种热塑性弹性体材料中空管状弹性体在提供完美支撑力的同时有良好的透气性除螨抑菌触感柔软，环保无味极易清洁。具有高强度，高回弹性等特点，由于拥有舒适的手感，广泛用于人造皮肤通过欧盟EU-10/2011食品级测试标准及欧盟RoHS环保安全标准。在睡眠时，头部保持低温，有效降低大脑皮层活跃度，利于大脑皮层休息。软管枕具有透气排热的特点，能够有效排出枕头内的热量，保持枕头内部温度提高睡眠质量',
-            'suitableCrowd': '颈椎不舒服，长时间坐姿工作压力大年轻人'
+            'suitableCrowd': '颈椎不舒服，长时间坐姿工作压力大年轻人',
+            'link':'lightgray'
         },
         'blue': {
             'name': '蓝色TPE弹体',
@@ -61,7 +64,8 @@ textureDetailPage.prototype.setData = function () {
             'color': 'Blue',
             'textureHead2': 'textureDetail-blue-hardness.png',
             'feature': 'PE+TPE弹性体是一种热塑性弹性体材料中空管状弹性体在提供完美支撑力的同时有良好的透气性除螨抑菌触感柔软，环保无味极易清洁。具有高强度，高回弹性等特点，由于拥有舒适的手感，广泛用于人造皮肤通过欧盟EU-10/2011食品级测试标准及欧盟RoHS环保安全标准。在睡眠时，头部保持低温，有效降低大脑皮层活跃度，利于大脑皮层休息。软管枕具有透气排热的特点，能够有效排出枕头内的热量，保持枕头内部温度提高睡眠质量',
-            'suitableCrowd': '颈椎不舒服，长时间坐姿工作压力大年轻人'
+            'suitableCrowd': '颈椎不舒服，长时间坐姿工作压力大年轻人',
+            'link':'blue'
         },
         'brown': {
             'name': '三棱荞麦壳',
@@ -72,7 +76,8 @@ textureDetailPage.prototype.setData = function () {
             'color': 'Dark Brown',
             'textureHead2': 'textureDetail-brown-hardness.png',
             'feature': '荞麦壳呈棱形，坚而不硬，凉爽舒适，壳与壳之间 自然形成完美透气通道，透气性佳，在地下埋藏百年不变质',
-            'suitableCrowd': '颈椎不舒服，长时间坐姿工作压力大年轻人'
+            'suitableCrowd': '颈椎不舒服，长时间坐姿工作压力大年轻人',
+            'link':'brown'
         },
         'gray': {
             'name': '竹炭弹性木+香木',
@@ -83,9 +88,10 @@ textureDetailPage.prototype.setData = function () {
             'color': 'Dark Brown',
             'textureHead2': 'textureDetail-gray-hardness.png',
             'feature': 'VCollection用高超的工艺将精心研发的竹炭素和 TPE弹性体一体成型，除具备细密多孔、抑菌驱虫 等特点天然的崖柏颗粒有对人身体有益的化学成分挥发油具有扩张支气管、抑菌、抗肿瘤、抗氧化等药理作用。在日本，柏木香气被誉为"空气维生素"，有癌症专家让病人在柏木林中保健治疗。用崖柏做熏香让崖柏的香气环绕在病人周围，可以改善病人心情，提高免疫力竹炭弹性体+天然的崖柏木颗粒，同时提供柔软和弹性对于是头部压力的两个中药体验因素。',
-            'suitableCrowd': '老少皆宜，尤其是易出汗体质的人群'
+            'suitableCrowd': '老少皆宜，尤其是易出汗体质的人群',
+            'link':'gray'
         }
-    }
+    };
 }
 
 //数据列表
@@ -145,7 +151,7 @@ textureDetailPage.prototype.setDataListHtml = function () {
     <!--底部按钮-->
     htmlArr.push('<div class="foot-btn">');
     htmlArr.push('<!--<p>马上了解产品</p>-->');
-    htmlArr.push('<a href="" class="btn">马上了解产品</a>');
+    htmlArr.push('<a href="../view/products-introduction.html?type='+data.link+'" class="btn">马上了解产品</a>');
     htmlArr.push('</div>');
 
     $('.main').html(htmlArr.join(''));
